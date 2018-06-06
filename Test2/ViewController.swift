9c74f7d9596a2d40e632b55f3f4c3e55902a5a23
Ellie Tiplady <Ellie.Tiplady@icloud.com>
2018-06-06 19:15:08 +0100

    Initial Commit

---
+++ Test2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Test2
+//
+//  Created by Ellie Tiplady on 06/06/2018.
+//  Copyright © 2018 Ellie Tiplady. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
